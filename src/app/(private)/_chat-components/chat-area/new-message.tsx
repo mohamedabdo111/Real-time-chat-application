69b1db26233af4024d15b00ee3615cbb8e49bc2d
mohamedabdo111 <moabdo346@gmail.com>
2025-03-20 19:16:55 +0200

    feat : Add emoji picker functionality to chat input

--- src/app/(private)/_chat-components/chat-area/new-message.tsx
+++ src/app/(private)/_chat-components/chat-area/new-message.tsx
@@ -4,13 +4,15 @@ import { IStateUser } from "@/redux/userSlice";
 import { CreateMessage } from "@/server-action/messages";
 import { Button, message } from "antd";
 import dayjs from "dayjs";
-import { create } from "domain";
+import EmojiPicker from "emoji-picker-react";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const NewMessage = () => {
   const [textmessage, setTextMessage] = React.useState<string>("");
+  const [showEmoji, setShowEmoji] = React.useState<boolean>(false);
   const { selectedChat }: IChatState = useSelector((state: any) => state.chat);
+  const inputRef = React.useRef<HTMLInputElement>(null);
   const { currentUserData }: IStateUser = useSelector(
     (state: any) => state.user
   );
@@ -31,6 +33,7 @@ const NewMessage = () => {
 
       socket.emit("send-new-message", socketPayload);
       setTextMessage("");
+      setShowEmoji(false);
 
       const dbPayload = {
         ...commonPayload,
@@ -53,11 +56,35 @@ const NewMessage = () => {
     }
   }, [selectedChat, textmessage]);
   return (
-    <div className="flex gap-3 p-3 border-0 border-t border-solid border-gray-200">
-      <div>{/* createdBy */}</div>
+    <div className="flex gap-3 p-3 border-0 border-t border-solid border-gray-200 relative">
+      {showEmoji && (
+        <div className="absolute left-2 bottom-16">
+          <EmojiPicker
+            height={350}
+            onEmojiClick={(e) => {
+              setTextMessage((prevText) => prevText + e.emoji);
+              inputRef.current?.focus();
+            }}
+          />
+        </div>
+      )}
+
+      <div>
+        <Button
+          className="border-gray-200 h-10 outline-none "
+          onClick={() => setShowEmoji(!showEmoji)}
+        >
+          {showEmoji ? (
+            <i className="ri-keyboard-line"></i>
+          ) : (
+            <i className="ri-emoji-sticker-line"></i>
+          )}
+        </Button>
+      </div>
       <div className=" flex-1">
         <input
           type="text"
+          ref={inputRef}
           className="w-full h-10 p-3 outline-none focus:outline-none border-gray-200 border-solid borcer-[1px] focus:border-gray-500  text-base"
           placeholder="Type a message"
           onChange={(e) => setTextMessage(e.target.value)}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import ThemeProvider from "@/providers/theme-provider";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import LayoutProvider from "@/providers/layout-provider";
+import "remixicon/fonts/remixicon.css";
 import ReduxProvider from "@/providers/redux-provider";
 export const metadata: Metadata = {
   title: "Create Next App",
